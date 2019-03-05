51126130820eaf31460867c128308f82d93e6d09
Mikayla <mtoffl01@users.noreply.github.com>
2019-03-05 11:03:31 -0500

    added useful comments for readability in script. commented out console log statements

--- liftknockoff/script.js
+++ liftknockoff/script.js
@@ -2,9 +2,11 @@ var myLat = 0;
 var myLng = 0;
 var me = new google.maps.LatLng(myLat, myLng);
 var myOptions = {
-    zoom: 13, // The larger the zoom number, the bigger the zoom
+    zoom: 13,
     center: me,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
+    //the code below is from google maps api website - makes the map "midnight"
+    //themed - for fun!
     styles: [
             {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
             {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
@@ -86,10 +88,17 @@ var myOptions = {
             }
           ]
 };
+
 var map;
 var marker;
-ridesLatLng = [];
-weinerLatLng = [];
+
+ridesLatLng = []; /*array of LatLng positions for "rides" - either
+vehicles or passengers, depending on the mode*/
+weinerLatLng = []; /*array of LatLng positions for weinermobile - sometimes
+two weinermobiles appear in the data, but will always just be accessing
+weinerLatLng[0]*/
+
+//container of all icons
 var icons = 
 {
     self :{
@@ -104,18 +113,19 @@ var icons =
     weinermobile: {
         icon: 'weinermobile.png'
     }
-};        
-var clients;
+};
+
+var clients; //container variable for passengers or vehicles
+
 var infowindow = new google.maps.InfoWindow();
 
 function init() {
-    console.log("Begin");
     map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
     getMyLocation();
 };
 
 function getMyLocation() {
-    if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
             myLat = position.coords.latitude;
             myLng = position.coords.longitude;
@@ -129,62 +139,53 @@ function getMyLocation() {
 
 function renderMap() {
     me = new google.maps.LatLng(myLat, myLng);
-    // Update map and go there...
     map.panTo(me);
                 
-    // Create a marker
+    // ME marker
     marker = new google.maps.Marker({
         position: me,
-        title: "Here I Am!",
+        title: "My icon",
         icon: 'betty_icon.jpg'
     });
     marker.setMap(map);
-                    
-    // Open info window on click of marker
+    
+    //get data (vehicles/passengers) from source...                
     getRides();
-    //constructInfoWindow();
 };
 
-//var weinermobile = false;
+//send request
 var request = new XMLHttpRequest();
 function getRides(){
     request.open("POST", "https://hans-moleman.herokuapp.com/rides", true);
     request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     request.onreadystatechange = function() {
         if (request.readyState == 4 && request.status == 200) {
-            console.log("Retrieved data!");
             data = request.responseText;
-            console.log(data);
+            //console.log(data);
             rides = JSON.parse(data);
-            if (rides.hasOwnProperty("vehicles")){
-                console.log("vehicle mode");
+            if (rides.hasOwnProperty("vehicles")){ //vehicles mode
                 clients = rides.vehicles;
-                for (count = 0; count< clients.length; count++){
+                for (count = 0; count< clients.length; count++){ //for all elts
                     if (clients[count].username == "WEINERMOBILE"){
-                        weiner_exists = true;
-                        //console.log("weiner found");
                         makeWeinerMarker(clients[count]);
                     }
                     else {
                         makeCarMarker(clients[count]);
                     }
                 }
-                constructInfoWindow("vehicle");
+                constructInfoWindow("vehicle"); //info window for vehicle mode
             }
-            else { //passengers 
-                console.log(mode);
+            else { //passengers mode
                 clients = rides.passengers;
                 for (count = 0; count< clients.length; count++){
                     if (clients[count].username == "WEINERMOBILE"){
-                        weiner_exists = true;
-                        //console.log("weiner found");
                         makeWeinerMarker(clients[count]);
                     }
                     else{
                         makePassengerMarker(clients[count]);
                     }
                 }
-                constructInfoWindow("passenger");
+                constructInfoWindow("passenger"); //info window passenger mode
             }
         }
         else if (request.readyState==4 && request.status != 200){
@@ -197,19 +198,20 @@ function getRides(){
     request.send("username=j3YRjYyc&lat=" + myLat + "&lng=" + myLng);
 };
 
+//takes in parameter of specific element which holds username, lat, lng...
 function makeWeinerMarker(vehicle)
 {
     var latLng = new google.maps.LatLng(vehicle.lat, vehicle.lng);
-    console.log("making weiner");
     var weinerMarker = new google.maps.Marker({
         position: latLng,
         title: vehicle._id,
-        //icon should be either weinermobile or car, depending on the data
         icon: 'weinermobile.png'
     })
     weinerMarker.setMap(map);
     weinerLatLng.push(latLng);
+    //compute distance between weinermobile and ME icon
     var dist = toMiles(google.maps.geometry.spherical.computeDistanceBetween(me, latLng));
+    //on click of weinermobile icon, display distance away from ME
     google.maps.event.addListener(weinerMarker, 'click', function()
     {
         infowindow.setContent(vehicle.username+" is "+dist+" miles away from you.")
@@ -217,19 +219,20 @@ function makeWeinerMarker(vehicle)
     });
 };
 
+//takes in parameter of specific element which holds username, lat, lng...
 function makeCarMarker(vehicle)
 {
     var latLng = new google.maps.LatLng(vehicle.lat, vehicle.lng);
-    console.log("making car");
     var carMarker = new google.maps.Marker({
         position: latLng,
         title: vehicle._id,
-        //icon should be either weinermobile or car, depending on the data
-        icon: 'car.png' //'car.png'
+        icon: 'car.png'
     })
     carMarker.setMap(map);
     ridesLatLng.push(latLng);
+    //compute distance between car and ME icon
     var dist = toMiles(google.maps.geometry.spherical.computeDistanceBetween(me, latLng));
+    //on click of car icon, display distance away from ME
     google.maps.event.addListener(carMarker, 'click', function()
     {
         infowindow.setContent(vehicle.username+" is "+dist+" miles away from you.")
@@ -243,7 +246,6 @@ function makePassengerMarker(pass)
     var passengerMarker = new google.maps.Marker({
         position: latLng,
         title: pass._id,
-        //icon should be either weinermobile or car, depending on the data
         icon: 'passenger.png'
     })
     passengerMarker.setMap(map);
@@ -259,24 +261,23 @@ function makePassengerMarker(pass)
 function constructInfoWindow(mode)
 {
     google.maps.event.addListener(marker, 'click', function() {
-        console.log("constructInfoWindow");
-        if (ridesLatLng.length > 0){
-            var dist = getNearestVehicle();
-            if (weinerLatLng.length >0)
+        if (ridesLatLng.length > 0){ //if vehicles/passengers exist
+            var dist = getNearest(); //call function to find distance to nearest car
+            if (weinerLatLng.length >0) //if weinermobile exists 
             {
                 var w_dist = getWeinerDist();
                 infowindow.setContent("Hello, j3YRjYyc. The nearest "+mode+" is "+dist+" miles away. The weinermobile is "+w_dist+"miles away.");
             }
-            else{
+            else{ //weinermobile doesn't exist, but vehicles/passengers do
                 infowindow.setContent("Hello, j3YRjYyc. The nearest "+mode+" is "+dist+" miles away. There is no weinermobile!")
             }
         }
-        else if (weinerLatLng.length>0)
+        else if (weinerLatLng.length>0) //vehicles/passengers don't exist, but weinermobile does
         {
             var w_dist = getWeinerDist();
             infowindow.setContent("Hello, j3YRjYyc. The weinermobile is "+w_dist +" miles away.");
         }
-        else 
+        else //no weinermobile or clients
         {
             infowindow.setContent("Hello, j3YRjYyc. No clients or weinermobile around!");
         }   
@@ -284,27 +285,27 @@ function constructInfoWindow(mode)
     });
 };
 
-function getNearestVehicle()
+//calculate distance to nearest vehicle or passenger
+function getNearest()
 {
     var dist = Number.MAX_VALUE;
     for (count = 0; count<ridesLatLng.length; count++){
         var temp = toMiles(google.maps.geometry.spherical.computeDistanceBetween(me, ridesLatLng[count]));
         if (temp < dist){
-            console.log("temp is "+temp);
             dist = temp;
         }
     }
-    console.log(" vehicle dist = "+ dist);
     return dist;
 };
 
+//calculate distance to weinermobile
 function getWeinerDist()
 {
     dist = toMiles(google.maps.geometry.spherical.computeDistanceBetween(me, weinerLatLng[0]));
-    console.log("weiner dist "+ dist);
     return dist;
 };
 
+//convert google.maps.geometry.spherical.computeDistanceBetween value from meters to miles
 function toMiles(dist)
 {
     return (0.000621371192 * dist).toFixed(2);
